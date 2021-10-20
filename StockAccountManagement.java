4598ece22c5acdc1f86ad85f1af62ac18aa843c7
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-20 18:00:12 +0530

    ADDED: To Check Stock Account Details

--- StockAccountManagement.java
+++ StockAccountManagement.java
@@ -5,11 +5,8 @@ public class StockAccountManagement {
 	public static void main(String[] args) {
 
 		// creating Object
-		StockPortfolio stockPortfolio = new StockPortfolio();
-		// Calling Method By Using Object
-		stockPortfolio.addStock("DMart", 2, 100);
-		stockPortfolio.addStock("Reliance", 3, 150);
-		stockPortfolio.calculateStockVal();
-		stockPortfolio.printReport();
+		StockAccount stockAccount = new StockAccount("Salman", 5000);
+		System.out.println("You have Succesfully Withdraw: " + stockAccount.debit(3500));
+		System.out.println("Your Current Account Balance is: " + stockAccount.checkBalance());
 	}
 }
