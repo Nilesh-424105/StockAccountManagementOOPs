cd0503655787af27dafb25e1687bb6eefb1b2eb1
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-20 17:29:16 +0530

    ADDED: Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.stock.account.management;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Stock Account Management Computation Program");
+	}
+
+}
